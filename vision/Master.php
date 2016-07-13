c0e57df1381ecf7eb77972cb0226a883b3ddb939
Xanfles <gonzalezpacheco.jonathan@gmail.com>
2016-07-13 01:01:26 -0400

    ejemplo para trabajar en front end

---
+++ vision/Master.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+	  <meta charset="utf-8">
+	  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	  <meta name="viewport" content="width=device-width, initial-scale=1">
+	  <meta name="description" content="">
+	  <meta name="author" content="">
+
+	  <title>Banco</title>
+
+	  <!-- Bootstrap Core CSS -->
+	  <link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+	  <!-- Custom CSS -->
+	  <link href="../bootstrap/css/sb-admin.css" rel="stylesheet">
+
+	  <!-- Morris Charts CSS -->
+	  <link href="../bootstrap/css/plugins/morris.css" rel="stylesheet">
+
+	  <!-- Custom Fonts -->
+	  <link href="../bootstrap/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+	</head>
+    <body>
+        <div id="wrapper">
+            <!-- Navigation -->
+            <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="Inicio.php">Banco</a>
+                </div>
+                <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
+                <div class="collapse navbar-collapse navbar-ex1-collapse">
+                    <ul class="nav navbar-nav side-nav">
+                        <li class="active">
+                            <a href="Ejemplo.php"><i class="fa fa-fw fa-dashboard" aria-hidden="true"></i>&nbsp;Ejemplo</a>
+                        </li>
+                        <li>
+                            <a href="../index.php" class="btn btn-danger btn-primary btn-cons">Desconectar</a>
+                        </li> 
+                    </ul>
+                </div>               
+            </nav>
+        	<div id="page-wrapper">
+          		<div class="container-fluid">
+            	<div id="subviewPlaceHolder_id">
+            </div>
+
+        </div>
+        <!-- jQuery -->
+        <script src="../bootstrap/js/jquery.js"></script>
+
+        <!-- Bootstrap Core JavaScript -->
+        <script src="../bootstrap/js/bootstrap.min.js"></script>
+
+        <!-- Morris Charts JavaScript -->
+        <script src="../bootstrap/js/plugins/morris/raphael.min.js"></script>
+        <script src="../bootstrap/js/plugins/morris/morris.min.js"></script>
+        <script src="../bootstrap/js/plugins/morris/morris-data.js"></script>
+    </body>
+</html>
